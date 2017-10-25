d995748ed93069a6821ecbb962ff063a7eaff09e
Aitor Guerrero <aguerrerolasarte@gmail.com>
2017-10-25 18:27:14 +0200

    default response

--- src/handler.functor.ts
+++ src/handler.functor.ts
@@ -3,13 +3,16 @@ import applyErrorLoggerMiddleware from './error-logger.middleware';
 import IHandler from './handler.interface';
 import applyInputLoggerMiddleware from './input-logger.middleware';
 import applyResponseLoggerMiddleware from './response-logger.middleware';
+import applyStandardResponse from './standard-response.middleware';
 
 export function buildHandler<E, R>(h: IAsyncHandler<E, R>): IHandler<E, R> {
 	return buildSyncHandler(
-		applyResponseLoggerMiddleware(
-			applyInputLoggerMiddleware(
-				applyErrorLoggerMiddleware(h, console),
-				console,
+		applyStandardResponse(
+			applyResponseLoggerMiddleware(
+				applyInputLoggerMiddleware(
+					applyErrorLoggerMiddleware(h, console),
+					console,
+				),
 			),
 		),
 	);
